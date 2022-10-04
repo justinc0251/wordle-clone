d9d0cc5e31a5185facaaea4c0f30dc48c63a44bb
Justin Chung <jdchung@scu.edu>
2022-10-04 00:59:51 -0700

    Add constant boardDefault

---
+++ src/Words.js
@@ -0,0 +1,10 @@
+// Attempts can be represented by an array of arrays
+// 6 tries with 5 letters for each try
+export const boardDefault = [
+    ["", "", "", "", "",],
+    ["", "", "", "", "",],
+    ["", "", "", "", "",],
+    ["", "", "", "", "",],
+    ["", "", "", "", "",],
+    ["", "", "", "", "",],
+];
