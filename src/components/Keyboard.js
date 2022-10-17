0f42ba5235024e853bb20e2d502d1d5dd854a583
Justin Chung <jdchung@scu.edu>
2022-10-17 01:06:54 -0700

    Create array for each line on keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Keyboard() {
-  return <div>Keyboard</div>;
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  return (
+    <div className="keyboard">
+      <div className="keyLine1"></div>
+      <div className="keyLine2"></div>
+      <div className="keyLine3"></div>
+    </div>
+  );
 }
 
 export default Keyboard;
