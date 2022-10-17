98c346ab4fc9653336c056c64a7ee6f6871cfae3
Justin Chung <jdchung@scu.edu>
2022-10-17 01:02:36 -0700

    Return letter const

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -5,7 +5,7 @@ function Letter({letterPosition, attemptNumber}) {
   // Context api for state management in order to keep track of letters on board
   const {board} = useContext(AppContext);
   const letter = board[attemptNumber][letterPosition];
-  return <div className="letter">letter</div>;
+  return <div className="letter">{letter}</div>;
 }
 
 export default Letter;
