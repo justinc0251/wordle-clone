851f5667dc6311cc85baeff416824618b3458ac5
Justin Chung <jdchung@scu.edu>
2022-10-24 02:12:44 -0700

    Add delete button functionality

--- src/components/Key.js
+++ src/components/Key.js
@@ -6,14 +6,34 @@ function Key({ keyVal, nonLetter }) {
   const { board, setBoard, currentAttempt, setCurrentAttempt } =
     useContext(AppContext);
   const selectLetter = () => {
-    if (currentAttempt.letterPos > 4) return;
-    const newBoard = [...board];
-    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal;
-    setBoard(newBoard);
-    setCurrentAttempt({
-      ...currentAttempt,
-      letterPosition: currentAttempt.letterPosition + 1,
-    });
+    if (keyVal === "ENTER") {
+      if (currentAttempt.letterPosition !== 5) return;
+      // Move down one row
+      setCurrentAttempt({
+        attempt: currentAttempt.attempt + 1,
+        letterPosition: 0,
+      });
+    } else if (keyVal === "⌫") {
+      // Can't delete first letter
+      if (currentAttempt.letterPosition === 0) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = "";
+      setBoard(newBoard);
+      setCurrentAttempt({
+        ...currentAttempt,
+        letterPosition: currentAttempt.letterPosition - 1,
+      });
+    } else {
+      if (currentAttempt.letterPosition > 4) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal;
+      setBoard(newBoard);
+      // Move to next column
+      setCurrentAttempt({
+        ...currentAttempt,
+        letterPosition: currentAttempt.letterPosition + 1,
+      });
+    }
   };
   return (
     <div className="key" id={nonLetter && "biggerBox"} onClick={selectLetter}>
